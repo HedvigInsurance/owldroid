0a37f31253d680698eff130c386858ecd00ae81a
Oscar Nylander <oscar@hedvig.com>
2019-02-26 09:59:14 +0100

    WIP new title looks for subscreens

--- owldroid/src/main/java/com/hedvig/android/owldroid/ui/profile/myinfo/MyInfoFragment.kt
+++ owldroid/src/main/java/com/hedvig/android/owldroid/ui/profile/myinfo/MyInfoFragment.kt
@@ -9,13 +9,12 @@ import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
 import com.hedvig.android.owldroid.R
 import com.hedvig.android.owldroid.di.ViewModelFactory
 import com.hedvig.android.owldroid.ui.profile.ProfileViewModel
 import com.hedvig.android.owldroid.util.compatSetTint
+import com.hedvig.android.owldroid.util.remove
+import com.hedvig.android.owldroid.util.show
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.activity_my_info.*
 import javax.inject.Inject
@@ -41,9 +40,9 @@ class MyInfoFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(
-            R.layout.activity_my_info,
-            container,
-            false
+                R.layout.activity_my_info,
+                container,
+                false
         )
 
         observeProfile()
@@ -56,10 +55,10 @@ class MyInfoFragment : Fragment() {
             val firstName = it!!.firstName().or("Test")
             val lastName = it.lastName().or("Testerson")
             val email = it.email().or("test@hedvig.com")
-            profile_my_info_loading_spinner.visibility = ProgressBar.GONE
-            profile_my_info_name_container.visibility = TextView.VISIBLE
+            profile_my_info_loading_spinner.remove()
+            profile_my_info_name_container.show()
             profile_my_info_sphere.drawable.compatSetTint(ContextCompat.getColor(context!!, R.color.dark_purple))
-            profile_my_info_contact_details_container.visibility = LinearLayout.VISIBLE
+            profile_my_info_contact_details_container.show()
 
             profile_my_info_name.text = "$firstName\n$lastName"
             profile_my_info_email.text = email
