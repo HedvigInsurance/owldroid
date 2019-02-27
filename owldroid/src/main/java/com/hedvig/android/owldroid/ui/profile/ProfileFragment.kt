700921c191b79c03fb1a50e259b4fa46517d6138
Johan Palmfjord <johan.palmfjord@hedvig.com>
2019-02-27 16:28:53 +0100

    More wip

--- owldroid/src/main/java/com/hedvig/android/owldroid/ui/profile/ProfileFragment.kt
+++ owldroid/src/main/java/com/hedvig/android/owldroid/ui/profile/ProfileFragment.kt
@@ -3,12 +3,15 @@ package com.hedvig.android.owldroid.ui.profile
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.LocalBroadcastManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import com.hedvig.android.owldroid.R
@@ -38,40 +41,56 @@ class ProfileFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.activity_profile, container, false)
-        observeProfile()
+        populateData()
         return view
     }
 
-    private fun observeProfile() {
+    private fun populateData() {
         profileViewModel.data.observe(this, Observer { profileData ->
             profile_loading_spinner.visibility = ProgressBar.GONE
             profile_rows_container.visibility = LinearLayout.VISIBLE
 
-            setupProfileRow(profileData!!)
-            setupPaymentRow(profileData)
+            setupMyInfoRow(profileData!!)
+            setupMyHomeRow(profileData)
+            setupCoinsured(profileData)
+            setupCharity(profileData)
+            setupPayment(profileData)
         })
     }
 
-    private fun setupPaymentRow(profileData: ProfileQuery.Data) {
-//        val cost = profileData.insurance().monthlyCost().or(0)
-//        profile_payment_monthly_cost.text = "$cost kr/månad · Betalas via autogiro"
-//        profile_payment_row.setOnClickListener {
-//            val intent = Intent("profileNavigation")
-//            intent.putExtra("subscreen", "payment")
-//            getLocalBroadcastManager().sendBroadcast(intent)
-//        }
+    private fun setupMyInfoRow(profileData: ProfileQuery.Data) {
+        val firstName = profileData.member().firstName().or("")
+        val lastName = profileData.member().lastName().or("")
+        profile_my_info_row.description = "$firstName $lastName"
+        attachNavigationOnClick(profile_my_info_row, "my_info")
     }
 
-    private fun setupProfileRow(profileData: ProfileQuery.Data) {
-//        val firstName = profileData.member().firstName().or("")
-//        val lastName = profileData.member().lastName().or("")
-//        profile_info_row_name.text = "$firstName $lastName"
-//        profile_my_info_row.setOnClickListener {
-//            val intent = Intent("profileNavigation")
-//            intent.putExtra("subscreen", "my_info")
-//            getLocalBroadcastManager().sendBroadcast(intent)
-//        }
+    private fun setupMyHomeRow(profileData: ProfileQuery.Data) {
+        profile_my_home_row.description = profileData.insurance().address().get()
+        attachNavigationOnClick(profile_my_home_row, "my_home")
     }
 
-    private fun getLocalBroadcastManager() = LocalBroadcastManager.getInstance(context!!)
+    private fun setupCoinsured(profileData: ProfileQuery.Data) {
+        val personsInHousehold = profileData.insurance().personsInHousehold().get()
+        profile_coinsured_row.description = "${personsInHousehold - 1} medförsäkrade"
+        attachNavigationOnClick(profile_coinsured_row, "coinsured")
+    }
+
+    private fun setupCharity(profileData: ProfileQuery.Data) {
+        profile_charity_row.description = profileData.cashback().name().get()
+        attachNavigationOnClick(profile_charity_row, "charity")
+    }
+
+    private fun setupPayment(profileData: ProfileQuery.Data) {
+        profile_payment_row.description = profileData.insurance().monthlyCost().get().toString() + "kr/månad - Betalas via autogiro"
+        attachNavigationOnClick(profile_payment_row, "payment")
+    }
+
+    private fun attachNavigationOnClick(view: View, subscreen: String) {
+        view.setOnClickListener {
+            val intent = Intent("profileNavigation")
+            intent.putExtra("subscreen", subscreen)
+            LocalBroadcastManager.getInstance(context!!).sendBroadcast(intent)
+        }
+    }
 }
--- owldroid/src/main/java/com/hedvig/android/owldroid/ui/profile/ProfileMenuRow.kt
+++ owldroid/src/main/java/com/hedvig/android/owldroid/ui/profile/ProfileMenuRow.kt
@@ -1,23 +1,66 @@
 package com.hedvig.android.owldroid.ui.profile
 
 import android.content.Context
-import android.support.v7.widget.LinearLayoutCompat
+import android.graphics.drawable.Drawable
+import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
-import android.widget.ImageView
 import android.widget.TextView
 import com.hedvig.android.owldroid.R
+import kotlinx.android.synthetic.main.profile_menu_row.view.*
 
-class ProfileMenuRow(context: Context, attributeSet: AttributeSet) : LinearLayoutCompat(context, attributeSet) {
-    init {
+class ProfileMenuRow : ConstraintLayout {
+    private var attributeSet: AttributeSet? = null
+    private var defStyle: Int = 0
+
+    constructor(
+        context: Context,
+        attributeSet: AttributeSet?,
+        defStyle: Int
+    ) : super(context, attributeSet, defStyle) {
+        this.attributeSet = attributeSet
+        this.defStyle = defStyle
         inflate(context, R.layout.profile_menu_row, this)
+        setupDynamicContent()
     }
+
+    constructor(
+        context: Context,
+        attributeSet: AttributeSet?
+    ) : super(context, attributeSet) {
+        this.attributeSet = attributeSet
+        inflate(context, R.layout.profile_menu_row, this)
+        setupDynamicContent()
+    }
+
+    constructor(context: Context) : super(context) {
+        inflate(context, R.layout.profile_menu_row, this)
+        setupDynamicContent()
+    }
+
+    var icon: Drawable? = null
+        set(value) {
+            field = value
+           profile_menu_row_icon.setImageDrawable(field)
+        }
+    var name: CharSequence? = null
+        set(value) {
+            field = value
+            profile_menu_row_name.text = field
+        }
+    var description: CharSequence? = null
+        set(value) {
+            field = value
+            profile_menu_row_description.text = field
+        }
 
     private fun setupDynamicContent() {
-        val attributesArray = arrayOf(R.attr.drawableSrc, R.attr.name, R.attr.description)
-        val attributes = context.obtainStyledAttributes(attributesArray.toIntArray())
+        val attributes = context.obtainStyledAttributes(attributeSet, R.styleable.ProfileMenuRow, defStyle, 0)
+
+        icon = attributes.getDrawable(R.styleable.ProfileMenuRow_iconImage)
+        name = attributes.getText(R.styleable.ProfileMenuRow_name)
+        description = attributes.getText(R.styleable.ProfileMenuRow_description)
+
         attributes.recycle()
-        findViewById<ImageView>(R.id.profile_menu_row_icon).imagedra = context.resources.getDrawable(attributes.getIndex(0))
-        findViewById<TextView>(R.id.profile_menu_row_name).setText(attributes.getIndex(1))
-        findViewById<TextView>(R.id.profile_menu_row_description).setText(attributes.getIndex(2))
     }
+
 }
