557d69a5988580487767eb03fb26c33d7d34ec2f
Oscar Nylander <oscar@hedvig.com>
2019-04-11 09:34:40 +0200

    Send broadcasts on action click
    This will later be picked up by a react native module and handled

--- owldroid/src/main/java/com/hedvig/android/owldroid/ui/dashboard/ActionAdapter.kt
+++ owldroid/src/main/java/com/hedvig/android/owldroid/ui/dashboard/ActionAdapter.kt
@@ -1,6 +1,8 @@
 package com.hedvig.android.owldroid.ui.dashboard
 
 import android.content.Context
+import android.content.Intent
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -24,9 +26,24 @@ class ActionAdapter(
 
     override fun onBindViewHolder(holder: ActionViewHolder, position: Int) {
         holder.button.text = items[position].text()
+        holder.button.setOnClickListener {
+            LocalBroadcastManager
+                .getInstance(context)
+                .sendBroadcast(Intent(DASHBOARD_NAVIGATION).also { intent ->
+                    intent.putExtra(ACTION, OPEN_CHAT)
+                    intent.putExtra(CHAT_ACTION_URL, items[position].triggerUrl())
+                })
+        }
     }
 
     class ActionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val button = view.dashboardChatAction
+    }
+
+    companion object {
+        const val DASHBOARD_NAVIGATION = "dashboardNavigation"
+        const val ACTION = "action"
+        const val OPEN_CHAT = "openChat"
+        const val CHAT_ACTION_URL = "chatAction"
     }
 }
