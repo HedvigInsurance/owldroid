25e2e43e9e7e74e2b0d552cd48d524b875e55e9f
Oscar Nylander <oscar@hedvig.com>
2019-04-30 17:23:03 +0200

    Bonus: add caching on asyncstorage

--- owldroid/src/main/java/com/hedvig/android/owldroid/util/react/AsyncStorageNative.kt
+++ owldroid/src/main/java/com/hedvig/android/owldroid/util/react/AsyncStorageNative.kt
@@ -1,7 +1,7 @@
 package com.hedvig.android.owldroid.util.react
 
 interface AsyncStorageNative {
-    fun getKey(key: String): String
+    fun getKey(key: String): String?
     fun deleteKey(key: String)
     fun setKey(key: String, value: String)
 }
