d9b93ca7ebc0b6749af09891b8888721d3d4c1bb
Oscar Nylander <oscar@hedvig.com>
2019-05-02 12:47:54 +0200

    fuck

--- owldroid/src/main/java/com/hedvig/android/owldroid/feature/dashboard/ui/PerilCategoryView.kt
+++ owldroid/src/main/java/com/hedvig/android/owldroid/feature/dashboard/ui/PerilCategoryView.kt
@@ -143,7 +143,7 @@ class PerilCategoryView : HedvigCardView {
             toggled = false
             return
         } else {
-            expandedContent?.animateExpand(updateCallBack = onAnimateExpand)
+            expandedContent?.animateExpand(updateCallback = onAnimateExpand)
             toggled = true
         }
     }
